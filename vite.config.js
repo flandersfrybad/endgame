8fbefed4df3bfb8f7f80b1b9391cb79da3e0bf7e
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-04-30 16:24:24 -0600

    crap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// filepath: c:\Users\Kate Fayad\Assembly Endgame\vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/endgame/',
+  base: '/',
 })
