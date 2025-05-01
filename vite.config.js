bf4f0db99112d2cde17053cda6227a28e986765f
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-04-30 19:38:45 -0600

    update 1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NETLIFY ? '/' : '/endgame/',
+  base: process.env.GITHUB_ACTIONS ? '/endgame/' : '/',
 })
